b7ca84405eed2da4cd2233a5747fb11eb1dbac96
its-nate <nmicinski@gmail.com>
2025-02-07 15:28:40 -0800

    cleanup

--- src/app/components/ClientLayout.jsx
+++ src/app/components/ClientLayout.jsx
@@ -1,15 +1,13 @@
 "use client";
+
 import { useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import M from "materialize-css";
 
-// const M = typeof window !== `undefined` ? require("materialize-css") : null;
-
 export default function ClientLayout({ children }) {
   useEffect(() => {
     setTimeout(function () {
-      console.log("Initializing Materialize");
       const sideNavElems = document.querySelectorAll(".sidenav");
       const sideNavInstances = M.Sidenav.init(sideNavElems, {
         preventScrolling: true,
