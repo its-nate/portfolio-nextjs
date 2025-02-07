c3971f7dc876be56bfab45dc67a592bb48ce58a2
its-nate <nmicinski@gmail.com>
2025-02-07 15:41:40 -0800

    simplify nav logic

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -13,99 +13,59 @@ const Header = () => {
       <ul id="slide-out" className="sidenav">
         <li>const nav = [</li>
         <li>
-          {activeNav === "home" ? (
-            <Link
-              href="/"
-              className="nav-item sidenav-close active-nav"
-              onClick={() => setActiveNav("home")}
-            >
-              home,
-            </Link>
-          ) : (
-            <Link
-              href="/"
-              className="nav-item sidenav-close"
-              onClick={() => setActiveNav("home")}
-            >
-              home,
-            </Link>
-          )}
+          <Link
+            href="/"
+            className={`nav-item sidenav-close ${
+              activeNav === "home" ? "active-nav" : ""
+            }`}
+            onClick={() => setActiveNav("home")}
+          >
+            home,
+          </Link>
         </li>
         <li>
-          {activeNav === "skills" ? (
-            <Link
-              href="/skills"
-              className="nav-item sidenav-close active-nav"
-              onClick={() => setActiveNav("skills")}
-            >
-              skills,
-            </Link>
-          ) : (
-            <Link
-              href="/skills"
-              className="nav-item sidenav-close"
-              onClick={() => setActiveNav("skills")}
-            >
-              skills,
-            </Link>
-          )}
+          <Link
+            href="/skills"
+            className={`nav-item sidenav-close ${
+              activeNav === "skills" ? "active-nav" : ""
+            }`}
+            onClick={() => setActiveNav("skills")}
+          >
+            skills,
+          </Link>
         </li>
         <li>
-          {activeNav === "code" ? (
-            <Link
-              href="/code"
-              className="nav-item sidenav-close active-nav"
-              onClick={() => setActiveNav("code")}
-            >
-              code,
-            </Link>
-          ) : (
-            <Link
-              href="/code"
-              className="nav-item sidenav-close"
-              onClick={() => setActiveNav("code")}
-            >
-              code,
-            </Link>
-          )}
+          <Link
+            href="/code"
+            className={`nav-item sidenav-close ${
+              activeNav === "code" ? "active-nav" : ""
+            }`}
+            onClick={() => setActiveNav("code")}
+          >
+            code,
+          </Link>
         </li>
         <li>
-          {activeNav === "about" ? (
-            <Link
-              href="/about"
-              className="nav-item sidenav-close active-nav"
-              onClick={() => setActiveNav("about")}
-            >
-              about,
-            </Link>
-          ) : (
-            <Link
-              href="/about"
-              className="nav-item sidenav-close"
-              onClick={() => setActiveNav("about")}
-            >
-              about,
-            </Link>
-          )}
+          <Link
+            href="/about"
+            className={`nav-item sidenav-close ${
+              activeNav === "about" ? "active-nav" : ""
+            }`}
+            onClick={() => setActiveNav("about")}
+          >
+            about,
+          </Link>
         </li>
         <li>
-          {activeNav === "contact" ? (
-            <Link
-              href="/contact"
-              className="nav-item sidenav-close active-nav"
-              onClick={() => setActiveNav("contact")}
-            >
-              contact
-            </Link>
-          ) : (
-            <Link
-              href="/contact"
-              className="nav-item sidenav-close"
-              onClick={() => setActiveNav("contact")}
-            >
-              contact
-            </Link>
-          )}
+          <Link
+            href="/contact"
+            className={`nav-item sidenav-close ${
+              activeNav === "contact" ? "active-nav" : ""
+            }`}
+            onClick={() => setActiveNav("contact")}
+          >
+            contact
+          </Link>
         </li>
         <li>]</li>
       </ul>
@@ -119,99 +79,59 @@ const Header = () => {
           <ul id="nav-mobile" className="hide-on-med-and-down">
             <li>const nav = [</li>
             <li>
-              {activeNav === "home" ? (
-                <Link
-                  href="/"
-                  className="nav-item active-nav"
-                  onClick={() => setActiveNav("home")}
-                >
-                  home,
-                </Link>
-              ) : (
-                <Link
-                  href="/"
-                  className="nav-item"
-                  onClick={() => setActiveNav("home")}
-                >
-                  home,
-                </Link>
-              )}
+              <Link
+                href="/"
+                className={`nav-item sidenav-close ${
+                  activeNav === "home" ? "active-nav" : ""
+                }`}
+                onClick={() => setActiveNav("home")}
+              >
+                home,
+              </Link>
             </li>
             <li>
-              {activeNav === "skills" ? (
-                <Link
-                  href="/skills"
-                  className="nav-item active-nav"
-                  onClick={() => setActiveNav("skills")}
-                >
-                  skills,
-                </Link>
-              ) : (
-                <Link
-                  href="/skills"
-                  className="nav-item"
-                  onClick={() => setActiveNav("skills")}
-                >
-                  skills,
-                </Link>
-              )}
+              <Link
+                href="/skills"
+                className={`nav-item sidenav-close ${
+                  activeNav === "skills" ? "active-nav" : ""
+                }`}
+                onClick={() => setActiveNav("skills")}
+              >
+                skills,
+              </Link>
             </li>
             <li>
-              {activeNav === "code" ? (
-                <Link
-                  href="/code"
-                  className="nav-item active-nav"
-                  onClick={() => setActiveNav("code")}
-                >
-                  code,
-                </Link>
-              ) : (
-                <Link
-                  href="/code"
-                  className="nav-item"
-                  onClick={() => setActiveNav("code")}
-                >
-                  code,
-                </Link>
-              )}
+              <Link
+                href="/code"
+                className={`nav-item sidenav-close ${
+                  activeNav === "code" ? "active-nav" : ""
+                }`}
+                onClick={() => setActiveNav("code")}
+              >
+                code,
+              </Link>
             </li>
             <li>
-              {activeNav === "about" ? (
-                <Link
-                  href="/about"
-                  className="nav-item active-nav"
-                  onClick={() => setActiveNav("about")}
-                >
-                  about,
-                </Link>
-              ) : (
-                <Link
-                  href="/about"
-                  className="nav-item"
-                  onClick={() => setActiveNav("about")}
-                >
-                  about,
-                </Link>
-              )}
+              <Link
+                href="/about"
+                className={`nav-item sidenav-close ${
+                  activeNav === "about" ? "active-nav" : ""
+                }`}
+                onClick={() => setActiveNav("about")}
+              >
+                about,
+              </Link>
             </li>
             <li>
-              {activeNav === "contact" ? (
-                <Link
-                  href="/contact"
-                  className="nav-item active-nav"
-                  onClick={() => setActiveNav("contact")}
-                >
-                  contact
-                </Link>
-              ) : (
-                <Link
-                  href="/contact"
-                  className="nav-item"
-                  onClick={() => setActiveNav("contact")}
-                >
-                  contact
-                </Link>
-              )}
+              <Link
+                href="/contact"
+                className={`nav-item sidenav-close ${
+                  activeNav === "contact" ? "active-nav" : ""
+                }`}
+                onClick={() => setActiveNav("contact")}
+              >
+                contact
+              </Link>
             </li>
             <li>]</li>
           </ul>
